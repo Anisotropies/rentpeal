b77fc96f19aa48f55bc087b24e392923608b9fa9
Anisotropies <Anisotropies@users.noreply.github.com>
2018-07-27 13:37:42 -0700

    adding mongodb connection

--- server.js
+++ server.js
@@ -26,3 +26,31 @@ var listener = app.listen(port, function () {
   console.log('Our app is running on http://localhost:' + port);
 });
 
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+
+var url = process.env.MONGOLAB_URI;
+
+var dbConn = mongodb.MongoClient.connect(url);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/post-feedback', function (req, res) {
+    dbConn.then(function(db) {
+        delete req.body._id; // for safety reasons
+        db.collection('feedbacks').insertOne(req.body);
+    });
+   //res.send('Data received:\n' + JSON.stringify(req.body));
+	res.end();
+});
+
+app.get('/view-feedbacks',  function(req, res) {
+    dbConn.then(function(db) {
+        db.collection('feedbacks').find({}).toArray().then(function(feedbacks) {
+            res.status(200).json(feedbacks);
+        });
+    });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
